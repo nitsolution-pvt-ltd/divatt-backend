6cde28a10d4420ca755acd60a05e6805288bd3d9
NITDEBANJAN <debanjan@nitsolution.in>
2022-03-17 17:43:56 +0530

    Database service application file config complete

--- category/src/main/java/com/divatt/category/Repository/CategoryRepo.java
+++ category/src/main/java/com/divatt/category/Repository/CategoryRepo.java
@@ -1,9 +1,9 @@
 package com.divatt.category.Repository;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
+
 
 import com.divatt.category.Entity.CategoryEntity;
-
-public interface CategoryRepo extends MongoRepository<CategoryEntity, Long>{
+public interface CategoryRepo{
 
 }
--- database-service-divatt/src/main/java/com/divatt/databaseservice/DatabaseServiceDivattApplication.java
+++ database-service-divatt/src/main/java/com/divatt/databaseservice/DatabaseServiceDivattApplication.java
@@ -2,8 +2,10 @@ package com.divatt.databaseservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DatabaseServiceDivattApplication {
 
 	public static void main(String[] args) {
