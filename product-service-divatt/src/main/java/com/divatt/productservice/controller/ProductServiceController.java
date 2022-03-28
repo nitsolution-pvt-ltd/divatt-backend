438eb2e98526ca69b23c59b250b60fb1e0592248
USER <user@USERs-MacBook-Air.local>
2022-03-28 12:06:40 +0530

    Product service

---
+++ product-service-divatt/src/main/java/com/divatt/productservice/controller/ProductServiceController.java
@@ -0,0 +1,10 @@
+package com.divatt.productservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductServiceController {
+
+}
