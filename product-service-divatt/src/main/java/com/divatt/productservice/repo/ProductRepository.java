757528e80e9044bbea975a428033d010d9bab2af
NITDEBANJAN <megodeye@gmail.com>
2022-03-30 13:52:08 +0530

    product service updated

--- product-service-divatt/src/main/java/com/divatt/productservice/repo/ProductRepository.java
+++ product-service-divatt/src/main/java/com/divatt/productservice/repo/ProductRepository.java
@@ -1,9 +1,10 @@
 package com.divatt.productservice.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.divatt.productservice.entity.ProductMasterEntity;
-import com.google.common.base.Optional;
 
 public interface ProductRepository extends MongoRepository<ProductMasterEntity, Integer>{
 
