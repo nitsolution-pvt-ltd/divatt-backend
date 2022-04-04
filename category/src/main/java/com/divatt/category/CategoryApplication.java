ee6802c8e02a1d94884f296082066286869c5e8d
ahadul02 <ahadul02@gmail.com>
2022-04-04 18:13:31 +0530

    Swagger

--- category/src/main/java/com/divatt/category/CategoryApplication.java
+++ category/src/main/java/com/divatt/category/CategoryApplication.java
@@ -3,12 +3,16 @@ package com.divatt.category;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 
 @SpringBootApplication
 @EnableEurekaClient
-
+@EnableWebMvc
+@EnableSwagger2
 public class CategoryApplication {
 
 	public static void main(String[] args) {
