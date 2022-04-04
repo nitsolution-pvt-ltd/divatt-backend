6c890da8a574b1b8eb28f3e3a1e9389fe12b4535
ahadul02 <ahadul02@gmail.com>
2022-04-04 18:25:08 +0530

    Swagger

--- product-service-divatt/src/main/java/com/divatt/productservice/ProductServiceDivattApplication.java
+++ product-service-divatt/src/main/java/com/divatt/productservice/ProductServiceDivattApplication.java
@@ -3,9 +3,14 @@ package com.divatt.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableWebMvc
+@EnableSwagger2
 public class ProductServiceDivattApplication {
 
 	public static void main(String[] args) {
