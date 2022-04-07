3f9006d68d01dc9c897df4b2e2ff0aa6219121a5
ahadul02 <ahadul02@gmail.com>
2022-04-07 15:15:17 +0530

    Category

---
+++ admin/src/main/java/com/divatt/admin/entity/CategoryEntity.java
@@ -0,0 +1,154 @@
+package com.divatt.admin.entity;
+
+import java.util.Date;
+import javax.validation.constraints.NotEmpty;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
+@Document(collection = "tbl_categories")
+public class CategoryEntity {
+	
+	@Id		  
+	private Integer id;
+
+	@Transient
+	public static final String SEQUENCE_NAME = "tbl_categories";
+	
+	@NotEmpty(message = "Category Name is Required")
+	private String categoryName;
+	@NotEmpty(message = "Category Descrition is Required")
+	private String categoryDescrition;
+	@NotEmpty(message = "Category Image is Required")
+	private String categoryImage;
+	private Integer level;
+	private Boolean isActive;
+	private Boolean isDeleted;	
+	private String parentId;
+	@JsonFormat(shape = Shape.STRING,pattern = "yyyy/MM/dd")
+	private Date createdOn;
+	private String createdBy;
+	
+	
+//	 @DBRef List<CategoryEntity> categoryEntity;
+//	 @Field("id")
+//		@DBRef(lazy = true)
+//		private Set<SubCategoryEntity> subCategoryEntity;
+//		private List<SubCategoryEntity> subCategoryEntity= new ArrayList<SubCategoryEntity>();
+	 
+	@Override
+	public String toString() {
+		return "CategoryEntity [id=" + id + ", categoryName=" + categoryName + ", categoryDescrition="
+				+ categoryDescrition + ", categoryImage=" + categoryImage + ", level=" + level + ", isActive="
+				+ isActive + ", isDeleted=" + isDeleted + ", parentId=" + parentId + ", createdOn=" + createdOn
+				+ ", createdBy=" + createdBy + "]";
+	}
+
+	public CategoryEntity() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public String getCategoryDescrition() {
+		return categoryDescrition;
+	}
+
+	public void setCategoryDescrition(String categoryDescrition) {
+		this.categoryDescrition = categoryDescrition;
+	}
+
+	public String getCategoryImage() {
+		return categoryImage;
+	}
+
+	public void setCategoryImage(String categoryImage) {
+		this.categoryImage = categoryImage;
+	}
+
+	public Integer getLevel() {
+		return level;
+	}
+
+	public void setLevel(Integer level) {
+		this.level = level;
+	}
+
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public Boolean getIsDeleted() {
+		return isDeleted;
+	}
+
+	public void setIsDeleted(Boolean isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+
+	public String getParentId() {
+		return parentId;
+	}
+
+	public void setParentId(String parentId) {
+		this.parentId = parentId;
+	}
+
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public static String getSequenceName() {
+		return SEQUENCE_NAME;
+	}
+//
+//	public List<SubCategoryEntity> getSubCategoryEntity() {
+//		return subCategoryEntity;
+//	}
+//
+//	public void setSubCategoryEntity(List<SubCategoryEntity> subCategoryEntity) {
+//		this.subCategoryEntity = subCategoryEntity;
+//	}
+
+
+	
+	
+
+	
+}
---
+++ admin/src/main/java/com/divatt/admin/repo/SubCategoryRepo.java
@@ -0,0 +1,31 @@
+package com.divatt.admin.repo;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.divatt.admin.entity.SubCategoryEntity;
+
+
+
+@Repository
+public interface SubCategoryRepo extends MongoRepository<SubCategoryEntity,Integer> {
+
+	Optional<SubCategoryEntity> findByCategoryName(String categoryName);
+	
+//	Page<SubCategoryEntity> findByIsDeleted(Boolean isDeleted,Pageable pagingSort);
+	Page<SubCategoryEntity> findByIsDeletedAndParentIdNot(Boolean isDeleted,String parentId,Pageable pagingSort);
+//	
+//	@Query("{'id' : ?0 , 'parentId' : ?1}")
+//	Page<SubCategoryEntity> findEmployeeByNameAndBand(String id, String parentId);
+//	
+	@Query(value = "{ $or: [ { 'categoryName' : {$regex:?0,$options:'i'} }, { 'categoryDescrition' : {$regex:?0,$options:'i'} },{ 'isActive' : {$regex:?0,$options:'i'} },{ 'categoryImage' : {$regex:?0,$options:'i'} },{ 'createdOn' : {$regex:?0,$options:'i'} } ],$and: [ { 'isDeleted' : ?1 }]}")
+    Page<SubCategoryEntity> SearchAndfindByParentIdNot(String sortKey, Boolean isDeleted, String parentId,Pageable pageable);
+
+	
+
+}
