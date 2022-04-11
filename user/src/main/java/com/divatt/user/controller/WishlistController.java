eed2f8de6eca6d681d7a4e269d7ff6b523aa574f
ahadul02 <ahadul02@gmail.com>
2022-04-11 12:30:51 +0530

    User Wishlist

--- user/src/main/java/com/divatt/user/controller/WishlistController.java
+++ user/src/main/java/com/divatt/user/controller/WishlistController.java
@@ -1,15 +1,22 @@
 package com.divatt.user.controller;
 
+import java.util.Map;
+import java.util.Optional;
+
 import javax.validation.Valid;
 
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.divatt.category.response.GlobalResponse;
@@ -17,44 +24,74 @@ import com.divatt.user.entity.wishlist.WishlistEntity;
 import com.divatt.user.exception.CustomException;
 import com.divatt.user.repository.wishlist.WishlistRepo;
 import com.divatt.user.services.WishlistService;
-
+import com.mashape.unirest.request.body.Body;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/wishlist")
 public class WishlistController {
-	
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(WishlistController.class);
-	
-	
-	
-	@Autowired WishlistRepo wishlistRepo;
-	
-	@Autowired WishlistService wishlistService;
-	
+
+	@Autowired
+	WishlistRepo wishlistRepo;
+
+	@Autowired
+	WishlistService wishlistService;
+
 	@PostMapping("/add")
 	public GlobalResponse postWishlistDetails(@Valid @RequestBody WishlistEntity wishlistEntity) {
 		LOGGER.info("Inside - WishlistController.postWishlistDetails()");
 
 		try {
-			 return this.wishlistService.postWishlistService(wishlistEntity);
+			return this.wishlistService.postWishlistService(wishlistEntity);
+		} catch (Exception e) {
+			throw new CustomException(e.getMessage());
+		}
+
+	}
+
+	@DeleteMapping("/delete")
+	public GlobalResponse deleteWishlistDetails(@RequestBody WishlistEntity wishlistEntity) {
+		LOGGER.info("Inside - WishlistController.deleteWishlistDetails()");
+
+		try {
+			return this.wishlistService.deleteWishlistService(wishlistEntity);
+		} catch (Exception e) {
+			throw new CustomException(e.getMessage());
+		}
+
+	}
+
+	@RequestMapping(value = { "/list" }, method = RequestMethod.GET)
+	public Map<String, Object> getWishlistDetails(@RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "10") int limit, @RequestParam(defaultValue = "DESC") String sort,
+			@RequestParam(defaultValue = "createdOn") String sortName,
+			@RequestParam(defaultValue = "false") Boolean isDeleted, @RequestParam(defaultValue = "") String keyword,
+			@RequestParam Optional<String> sortBy) {
+		LOGGER.info("Inside - WishlistController.getWishlistDetails()");
+
+		try {
+			return this.wishlistService.getWishlistDetails(page, limit, sort, sortName, keyword, sortBy);
 		} catch (Exception e) {
 			throw new CustomException(e.getMessage());
 		}
 
 	}
 	
-	@DeleteMapping("/delete")
-	public GlobalResponse deleteWishlistDetails(@RequestBody WishlistEntity wishlistEntity) {
-		LOGGER.info("Inside - WishlistController.deleteWishlistDetails()");
+	
+	@RequestMapping(value = { "/list-rest" }, method = RequestMethod.GET)
+	public ResponseEntity<?> getWishlistRestDetails(@RequestParam(defaultValue = "") Integer userId) {
+		LOGGER.info("Inside - WishlistController.getWishlistRestDetails()");
 
 		try {
-			 return this.wishlistService.deleteWishlistService(wishlistEntity);
+			return this.wishlistService.getWishlistRestDetails(userId);
 		} catch (Exception e) {
 			throw new CustomException(e.getMessage());
 		}
 
 	}
+	
+	
 
 }
--- user/src/main/java/com/divatt/user/services/WishlistService.java
+++ user/src/main/java/com/divatt/user/services/WishlistService.java
@@ -1,6 +1,10 @@
 package com.divatt.user.services;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.validation.Valid;
@@ -8,28 +12,38 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriUtils;
 
 import com.divatt.category.response.GlobalResponse;
 import com.divatt.user.entity.wishlist.WishlistEntity;
 import com.divatt.user.exception.CustomException;
 import com.divatt.user.repository.wishlist.WishlistRepo;
-
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.request.body.RequestBodyEntity;
 
 @Service
 public class WishlistService {
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(WishlistService.class);
-	@Autowired 
+	@Autowired
 	WishlistRepo wishlistRepo;
-	
+
 	@Autowired
 	SequenceGenerator sequenceGenerator;
-	
-	
+
 	public GlobalResponse postWishlistService(WishlistEntity wishlistEntity) {
-			LOGGER.info("Inside - WishlistService.postWishlistService()");
-		
+		LOGGER.info("Inside - WishlistService.postWishlistService()");
+
 		try {
 			Optional<WishlistEntity> findByCategoryName = wishlistRepo.findByProductId(wishlistEntity.getProductId());
 			if (findByCategoryName.isPresent()) {
@@ -37,34 +51,113 @@ public class WishlistService {
 			} else {
 				WishlistEntity filterCatDetails = new WishlistEntity();
 
-				
 				filterCatDetails.setId(sequenceGenerator.getNextSequence(wishlistEntity.SEQUENCE_NAME));
-				filterCatDetails.setUserId(wishlistEntity.getUserId());				
+				filterCatDetails.setUserId(wishlistEntity.getUserId());
 				filterCatDetails.setProductId(wishlistEntity.getProductId());
 				filterCatDetails.setAddedOn(new Date());
-						
-			
-				wishlistRepo.save(filterCatDetails);				
-				return new GlobalResponse("SUCCESS", "Wishlist Added Succesfully", 200);				
+
+				wishlistRepo.save(filterCatDetails);
+				return new GlobalResponse("SUCCESS", "Wishlist Added Succesfully", 200);
 			}
-			
+
 		} catch (Exception e) {
 			throw new CustomException(e.getMessage());
 		}
-		
-	}
 
+	}
 
 	public GlobalResponse deleteWishlistService(@Valid WishlistEntity wishlistEntity) {
 		try {
 			Optional<WishlistEntity> findByProductRow = wishlistRepo.findById(wishlistEntity.getId());
 			if (!findByProductRow.isPresent()) {
 				return new GlobalResponse("ERROR", "Product Not Exists!", 200);
-			} else {							
-				wishlistRepo.deleteById(wishlistEntity.getId());				
-				return new GlobalResponse("SUCCESS", "Wishlist Reomved Succesfully", 200);				
+			} else {
+				wishlistRepo.deleteById(wishlistEntity.getId());
+				return new GlobalResponse("SUCCESS", "Wishlist Reomved Succesfully", 200);
 			}
+
+		} catch (Exception e) {
+			throw new CustomException(e.getMessage());
+		}
+
+	}
+
+	public Map<String, Object> getWishlistDetails(int page, int limit, String sort, String sortName, String keyword,
+			Optional<String> sortBy) {
+		try {
+			int CountData = (int) wishlistRepo.count();
+			Pageable pagingSort = null;
+			if (limit == 0) {
+				limit = CountData;
+			}
+
+			if (sort.equals("ASC")) {
+				pagingSort = PageRequest.of(page, limit, Sort.Direction.ASC, sortBy.orElse(sortName));
+			} else {
+				pagingSort = PageRequest.of(page, limit, Sort.Direction.DESC, sortBy.orElse(sortName));
+			}
+
+			Page<WishlistEntity> findAll = null;
+
+			if (keyword.isEmpty()) {
+				findAll = wishlistRepo.findAll(pagingSort);
+			} else {
+				findAll = wishlistRepo.Search(keyword, pagingSort);
+
+			}
+
+			int totalPage = findAll.getTotalPages() - 1;
+			if (totalPage < 0) {
+				totalPage = 0;
+			}
+
+			Map<String, Object> response = new HashMap<>();
+			response.put("data", findAll.getContent());
+			response.put("currentPage", findAll.getNumber());
+			response.put("total", findAll.getTotalElements());
+			response.put("totalPage", totalPage);
+			response.put("perPage", findAll.getSize());
+			response.put("perPageElement", findAll.getNumberOfElements());
+
+			if (findAll.getSize() <= 1) {
+				throw new CustomException("Wishlist Not Found!");
+			} else {
+				return response;
+			}
+		} catch (Exception e) {
+			throw new CustomException(e.getMessage());
+		}
+	}
+
+	public ResponseEntity<?> getWishlistRestDetails(Integer userId) throws UnirestException {
+
+		try {
 			
+			List<WishlistEntity> findByUserId = wishlistRepo.findByUserId(userId);
+			List<Integer> productIds = new ArrayList<>();
+//			LOGGER.info("Inside - WishlistController.getWishlistRestDetaildds()"+findByUserId);
+	
+			findByUserId.forEach((e)->{
+				productIds.add(e.getProductId());
+			});
+//			
+//			productIds.forEach((e)->{System.out.println("E   "+ e);});
+			LOGGER.info("Inside - WishlistController.getWishlistRestDetaildds()"+productIds);
+
+			HttpHeaders headers = new HttpHeaders();
+//			headers.set("Authorization", token);
+//			headers.setContentType(MediaType.APPLICATION_JSON);
+			HttpEntity request = new HttpEntity(headers);
+
+//			String encode = UriUtils.encodePath(processedSMSBodyContent, "UTF-8");
+//			HttpResponse<JsonNode> asJson = null;
+			Unirest.setTimeouts(0, 0);
+			RequestBodyEntity body = Unirest.post("http://192.168.29.42:8083/dev/product/getProductList")
+				  .header("Content-Type", "application/json")
+				  .body(productIds.toString());
+			return ResponseEntity.ok(body.getBody());
+//			 LOGGER.info("Inside - WishlistController.getWishlistRestDetaildds(jjjj)"+ok);
+//return null;
 		} catch (Exception e) {
 			throw new CustomException(e.getMessage());
 		}
