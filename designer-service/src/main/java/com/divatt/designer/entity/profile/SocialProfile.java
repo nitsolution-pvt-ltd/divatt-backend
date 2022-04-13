d271692fda68a572f82520b7ddb5ad086d3ae349
soumendoluinit <soumen.dolui@nitsolution.in>
2022-04-13 12:50:50 +0530

    Designer Registration

---
+++ designer-service/src/main/java/com/divatt/designer/entity/profile/SocialProfile.java
@@ -0,0 +1,78 @@
+package com.divatt.designer.entity.profile;
+
+import org.springframework.data.mongodb.core.mapping.Field;
+
+public class SocialProfile {
+	
+	@Field(name = "facebook_link")
+	private String facebookLink;
+	@Field(name = "instagram_link")
+	private String instagramLink;
+	@Field(name = "youtube_link")
+	private String youtubeLink;
+	@Field(name = "description")
+	private String description;
+	@Field(name = "address")
+	private String address;
+	@Field(name = "achievements")
+	private String achievements;
+	public SocialProfile() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public SocialProfile(String facebookLink, String instagramLink, String youtubeLink, String description,
+			String address, String achievements) {
+		super();
+		this.facebookLink = facebookLink;
+		this.instagramLink = instagramLink;
+		this.youtubeLink = youtubeLink;
+		this.description = description;
+		this.address = address;
+		this.achievements = achievements;
+	}
+	@Override
+	public String toString() {
+		return "SocialProfile [facebookLink=" + facebookLink + ", instagramLink=" + instagramLink + ", youtubeLink="
+				+ youtubeLink + ", description=" + description + ", address=" + address + ", achievements="
+				+ achievements + "]";
+	}
+	public String getFacebookLink() {
+		return facebookLink;
+	}
+	public void setFacebookLink(String facebookLink) {
+		this.facebookLink = facebookLink;
+	}
+	public String getInstagramLink() {
+		return instagramLink;
+	}
+	public void setInstagramLink(String instagramLink) {
+		this.instagramLink = instagramLink;
+	}
+	public String getYoutubeLink() {
+		return youtubeLink;
+	}
+	public void setYoutubeLink(String youtubeLink) {
+		this.youtubeLink = youtubeLink;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getAchievements() {
+		return achievements;
+	}
+	public void setAchievements(String achievements) {
+		this.achievements = achievements;
+	}
+	
+	
+
+}
---
+++ designer-service/src/main/java/com/divatt/designer/repo/DesignerProfileRepo.java
@@ -0,0 +1,9 @@
+package com.divatt.designer.repo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.divatt.designer.entity.profile.DesignerProfileEntity;
+
+public interface DesignerProfileRepo extends MongoRepository<DesignerProfileEntity, Long>{
+
+}
