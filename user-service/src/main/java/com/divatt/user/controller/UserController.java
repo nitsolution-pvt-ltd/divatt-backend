fa60dd52be6a1af995fa5193a2f8e313c4f171ae
soumendoluinit <soumen.dolui@nitsolution.in>
2022-04-21 18:14:44 +0530

    User prof update

--- user-service/src/main/java/com/divatt/user/controller/UserController.java
+++ user-service/src/main/java/com/divatt/user/controller/UserController.java
@@ -287,5 +287,47 @@ public class UserController {
 	    httpServletResponse.setHeader("Location", env.getProperty("redirecturl"));
 	    httpServletResponse.setStatus(302);
 	}
+	
+	@PostMapping("/update")
+	public ResponseEntity<?> updateUser(@Valid @RequestBody UserLoginEntity userLoginEntityParam, Errors error) {
+		LOGGER.info("Inside - UserController.addUser()");
+		try {
+			if (error.hasErrors()) {
+				throw new CustomException("Check The Fields");
+			}
+			Optional<UserLoginEntity> findById = userLoginRepo.findById(userLoginEntityParam.getuId());
+			if (findById.isPresent())
+				throw new CustomException("User not found");
+			SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy-HH:mm:ss");
+			Date date = new Date();
+			formatter.format(date);
+			UserLoginEntity userLoginEntity = findById.get();
+			userLoginEntity.setFirstName(userLoginEntityParam.getFirstName());
+			userLoginEntity.setLastName(userLoginEntityParam.getLastName());
+			userLoginEntity.setMobileNo(userLoginEntityParam.getMobileNo());
+			userLoginEntity.setDob(userLoginEntityParam.getDob());
+			userLoginRepo.save(userLoginEntity);
+			JsonObject jo = new JsonObject();
+			jo.addProperty("senderMailId", userLoginEntity.getEmail());
+			jo.addProperty("subject", "Successfully Registration");
+			jo.addProperty("body", "Welcome " + userLoginEntity.getEmail() + ""
+					+ ",\n &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp"
+					+ " you have been register successfully."+"Please active your account by clicking the bellow link "+ URI.create(env.getProperty("redirectapi")+ Base64.getEncoder().encodeToString(userLoginEntity.getEmail().toString().getBytes()))  + " . We will verify your details and come back to you soon." );
+			jo.addProperty("enableHtml", false);
+			try {
+				Unirest.setTimeouts(0, 0);
+				HttpResponse<String> response = Unirest.post("http://localhost:8080/dev/auth/sendMail")
+				  .header("Content-Type", "application/json")
+				  .body(jo.toString())
+				  .asString();
+			}catch(Exception e) {
+				
+			}
+			return ResponseEntity.ok(new GlobalResponse("SUCCESS", "Updated Successfully", 200));
+		} catch (Exception e) {
+			throw new CustomException(e.getMessage());
+		}
+
+	}
 
 }
