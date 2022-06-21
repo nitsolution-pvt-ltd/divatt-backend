ae71b052a7ffb4c980ac51faf11c0f211a4b36d0
NITDEBANJAN <megodeye@gmail.com>
2022-06-21 19:44:33 +0530

    Requler push pull

--- designer-service/src/main/java/com/divatt/designer/helper/CustomFunction.java
+++ designer-service/src/main/java/com/divatt/designer/helper/CustomFunction.java
@@ -159,6 +159,7 @@ public class CustomFunction {
 			filterProductEntity.setProductName(productData.getProductName());
 			filterProductEntity.setPurchaseQuantity(productData.getPurchaseQuantity());
 			filterProductEntity.setSpecifications(productData.getSpecifications());
+			filterProductEntity.setStanderedSOH(productData.getStanderedSOH());
 //			filterProductEntity.setStanderedSOH(productData.getStanderedSOH());
 			filterProductEntity.setSubCategoryId(productData.getSubCategoryId());
 			filterProductEntity.setAdminStatusOn(productData.getAdminStatusOn());
