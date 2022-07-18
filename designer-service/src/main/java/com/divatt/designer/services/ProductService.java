342ffab9a3ac06bf179758b097bcc1f44dd1b5f5
NITDEBANJAN <megodeye@gmail.com>
2022-07-18 15:33:23 +0530

    Reguler push pull

--- designer-service/src/main/java/com/divatt/designer/services/ProductService.java
+++ designer-service/src/main/java/com/divatt/designer/services/ProductService.java
@@ -190,7 +190,6 @@ public class ProductService {
 						//System.out.println(userInfo);
 						userInfoList.add(userInfo.getBody());
 					}
-<<<<<<< HEAD
 					emailThreadClass.emailThreadRun(userId);
 					System.out.println("Main Class");
 					for(int i=0;i<userId.size();i++)
@@ -199,9 +198,6 @@ public class ProductService {
 						
 					}
 					productRepo.save(customFunction.filterDataEntity(productData));
-=======
->>>>>>> 64bb9867e7a171bf6f37e84cac862d09f02bf4fc
-					
 					//char emailData[];
 					ImagesEntity[] images=productData.getImages();
 					String image1=images[0].getName();
