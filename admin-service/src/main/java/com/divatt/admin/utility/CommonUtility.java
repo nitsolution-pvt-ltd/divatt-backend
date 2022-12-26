f8773263ab76b9ff56d800ee4a5c2fde711a3b7c
ahadul02 <ahadul@nitsolution.in>
2022-12-26 17:25:41 +0530

    Excel added for accounts

--- admin-service/src/main/java/com/divatt/admin/utility/CommonUtility.java
+++ admin-service/src/main/java/com/divatt/admin/utility/CommonUtility.java
@@ -1,13 +1,22 @@
 package com.divatt.admin.utility;
 
+import java.text.DecimalFormat;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-
 @Component
 public class CommonUtility {
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(CommonUtility.class);
+
+	public static double duoble(float f) {
+
+		DecimalFormat df = new DecimalFormat("0.00");
+		Double valueOf = Double.valueOf(df.format(f));
+		return valueOf;
+ 
+	}
 
 }
